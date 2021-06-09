d7c67a2a3c329078263bbfcc903ee8bd8fcd6e82
Justin Ochoa <just.och92@gmail.com>
2021-06-09 13:51:07 -0400

    first commit

---
+++ index.js
@@ -0,0 +1,8 @@
+mapboxgl.accessToken =
+  "pk.eyJ1Ijoiam9jaG9hNTc5MiIsImEiOiJja2dpa2ZiMmswaHpmMnpuczQwd3FyOGFoIn0.yOGrduGvq2m0gsfRFl1F9A";
+var map = new mapboxgl.Map({
+  container: "map",
+  style: "mapbox://styles/mapbox/streets-v11",
+  center: [-71.060982, 42.35725],
+  zoom: 18,
+});
